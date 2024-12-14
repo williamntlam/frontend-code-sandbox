9d3e4031667766e10e741fe07e6a34ffa40a5d12
William Lam <williamntlam@gmail.com>
2024-12-14 18:23:38 -0500

    feat: added initial structure for the Dashboard component.

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -5,8 +5,19 @@ export default function Dashboard() {
         {/* Options: Projects, Folders, Recently Deleted */}
         {/* Can also display the User Icon + name, etc when user authentification is added. */}
         This is the sidebar.
+        <section>Profile Section</section>
+        <section>
+          Navigation Buttons
+          <nav>
+            <ul>
+              <li>Projects</li>
+              <li>Recently Deleted</li>
+            </ul>
+          </nav>
+        </section>
       </aside>
       <main>
+        {/* Two pages to appear: Projects + Recently Deleted */}
         This is the main content that is displayed based on what is selected on
         the sidebar.
       </main>
