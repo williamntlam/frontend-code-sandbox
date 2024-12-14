547d5341c859e3be214aa6733ced1acdbaf89c73
William Lam <williamntlam@gmail.com>
2024-12-14 16:24:14 -0500

    feat: created Dashboard component for dashboard page.

---
+++ pages/dashboard.tsx
@@ -0,0 +1,15 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <aside>
+        {/* Options: Projects, Folders, Recently Deleted */}
+        {/* Can also display the User Icon + name, etc when user authentification is added. */}
+        This is the sidebar.
+      </aside>
+      <main>
+        This is the main content that is displayed based on what is selected on
+        the sidebar.
+      </main>
+    </div>
+  );
+}
