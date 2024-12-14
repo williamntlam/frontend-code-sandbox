56daf73f7622abfee69f43d292fca78f5efd65c6
William Lam <williamntlam@gmail.com>
2024-12-13 20:43:01 -0500

    feat: implemented typing animation for featured frameworks + frontend languages on the hero page; implemented colourful title and intro description for the hero page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,114 +1,88 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import React from "react";
+import { ReactTyped } from "react-typed";
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <main>
+        <section className="text-center">
+          {/* Title and introduction description for UIPlayground. */}
+          <h1 className="text-7xl">
+            Welcome to{" "}
+            <div className="text-6xl font-bold">
+              <span className="text-red-500">U</span>
+              <span className="text-orange-500">I</span>
+              <span className="text-yellow-500">P</span>
+              <span className="text-green-500">l</span>
+              <span className="text-teal-500">a</span>
+              <span className="text-blue-500">y</span>
+              <span className="text-indigo-500">g</span>
+              <span className="text-purple-500">r</span>
+              <span className="text-pink-500">o</span>
+              <span className="text-red-500">u</span>
+              <span className="text-orange-500">n</span>
+              <span className="text-yellow-500">d</span>
+              <span className="text-green-500">!</span>
+            </div>
+          </h1>
+          <p>
+            UIPlayground is a dynamic and interactive frontend sandbox coding
+            application designed for developers of all levels. Whether you're a
+            seasoned pro or just starting out, UIPlayground provides a versatile
+            environment where you can experiment with user interface designs,
+            test frontend code, and prototype new ideas in real-time.
+          </p>
+        </section>
+        <section className="flex flex-col gap-2 items-center font-bold text-6xl">
+          {/* Typography of Major Languages like HTML, CSS, React, Angular, and JavaScript. */}
+          {/* Type the languages in and out, in different colours. */}
+          <ReactTyped
+            strings={["HTML\n"]}
+            typeSpeed={100}
+            loop
+            backSpeed={100}
+            showCursor={true}
+            className="text-red-600"
+          />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+          <ReactTyped
+            strings={["CSS\n"]}
+            typeSpeed={200}
+            loop
+            backSpeed={200}
+            showCursor={true}
+            className="text-blue-600"
+          />
+          <ReactTyped
+            strings={["React\n"]}
+            typeSpeed={300}
+            loop
+            backSpeed={300}
+            showCursor={true}
+            className="text-purple-600"
+          />
+          <ReactTyped
+            strings={["Angular\n"]}
+            typeSpeed={250}
+            loop
+            backSpeed={250}
+            showCursor={true}
+            className="text-orange-600"
+          />
+          <ReactTyped
+            strings={["JavaScript\n"]}
+            typeSpeed={200}
+            loop
+            backSpeed={200}
+            showCursor={true}
+            className="text-emerald-600"
+          />
+        </section>
+        <section>
+          {/* This is for the 'Get Started' button. */}
+          <button>Get Started</button>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
