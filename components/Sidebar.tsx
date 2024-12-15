5269460a4a6d487c803ac4cb7484f6cf76ba4572
William Nhut Lam <163342709+williamntlam@users.noreply.github.com>
2024-12-15 17:45:25 +0000

    feat: added temporary profile picture in sidebar component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,10 +1,13 @@
+import Image from "next/image";
+
 export default function Sidebar() {
   return (
-    <aside>
+    <aside className="w-64 h-full bg-gray-800 text-white fixed">
       {/* Options: Projects, Folders, Recently Deleted */}
       {/* Can also display the User Icon + name, etc when user authentification is added. */}
-      This is the sidebar.
-      <section>Profile Section</section>
+      <section>
+        <Image src="/profile.png" width={350} height={350} className="p-10" />
+      </section>
       <section>
         Navigation Buttons
         <nav>
