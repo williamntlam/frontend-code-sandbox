b84d143407214f2209a690368194ace7111aff91
William Lam <williamntlam@gmail.com>
2024-12-16 16:57:22 -0500

    feat: added navigation button to go back to the hero page in the sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,54 +3,64 @@ import Link from "next/link";
 
 export default function Sidebar() {
   return (
-    <aside className="w-64 h-full bg-gray-800 text-white">
+    <aside className="w-64 h-full bg-gray-800 text-white flex flex-col justify-between">
       {/* Options: Projects, Folders, Recently Deleted */}
-      {/* Can also display the User Icon + name, etc when user authentification is added. */}
-      <section className="text-center p-10">
-        <div className="flex flex-col items-center">
-          <Image
-            src="/profile.png"
-            width={350}
-            height={350}
-            alt="Profile Picture"
-          />
-          <h2 className="mt-4 text-xl font-semibold">Hello, William Lam!</h2>
-        </div>
-      </section>
+      {/* Can also display the User Icon + name, etc when user authentication is added. */}
+      <div>
+        <section className="text-center p-10">
+          <div className="flex flex-col items-center">
+            <Image
+              src="/profile.png"
+              width={350}
+              height={350}
+              alt="Profile Picture"
+            />
+            <h2 className="mt-4 text-xl font-semibold">Hello, William Lam!</h2>
+          </div>
+        </section>
 
-      <section className="text-center">
-        <nav>
-          <ul className="space-y-4">
-            <li>
-              <Link href="/dashboard/projects">
-                <button className="hover:text-blue-600 transition-colors duration-900">
-                  Projects
-                </button>
-              </Link>
-            </li>
-            <li>
-              <Link href="/dashboard/starred-projects">
-                <button className="hover:text-blue-600 transition-colors duration-900">
-                  Starred Projects
-                </button>
-              </Link>
-            </li>
-            <li>
-              <Link href="/dashboard/templates">
-                <button className="hover:text-blue-600 transition-colors duration-900">
-                  Templates
-                </button>
-              </Link>
-            </li>
-            <li>
-              <Link href="recently-deleted">
-                <button className="hover:text-blue-600 transition-colors duration-900">
-                  Recently Deleted
-                </button>
-              </Link>
-            </li>
-          </ul>
-        </nav>
+        <section className="text-center">
+          <nav>
+            <ul className="space-y-4">
+              <li>
+                <Link href="/dashboard/projects">
+                  <button className="hover:text-blue-600 transition-colors duration-900">
+                    Projects
+                  </button>
+                </Link>
+              </li>
+              <li>
+                <Link href="/dashboard/starred-projects">
+                  <button className="hover:text-blue-600 transition-colors duration-900">
+                    Starred Projects
+                  </button>
+                </Link>
+              </li>
+              <li>
+                <Link href="/dashboard/templates">
+                  <button className="hover:text-blue-600 transition-colors duration-900">
+                    Templates
+                  </button>
+                </Link>
+              </li>
+              <li>
+                <Link href="/dashboard/recently-deleted">
+                  <button className="hover:text-blue-600 transition-colors duration-900">
+                    Recently Deleted
+                  </button>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </section>
+      </div>
+
+      <section className="text-center p-4">
+        <Link href="/">
+          <button className="hover:text-blue-600 transition-colors duration-900">
+            UIPlayground
+          </button>
+        </Link>
       </section>
     </aside>
   );
