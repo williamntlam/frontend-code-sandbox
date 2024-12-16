a6ffd36514503be3cf33290212f0df29e4f88faf
William Lam <williamntlam@gmail.com>
2024-12-16 16:25:48 -0500

    feat: implemented dynamic routing for dashboard

--- pages/dashboard/[section].tsx
+++ pages/dashboard/[section].tsx
@@ -1,8 +1,21 @@
 import { useRouter } from "next/router";
+import Projects from "@/components/dashboard/Projects";
+import StarredProjects from "@/components/dashboard/StarredProjects";
+import Templates from "@/components/dashboard/Templates";
+import RecentlyDeleted from "@/components/dashboard/RecentlyDeleted";
+import Sidebar from "@/components/Sidebar";
 
 export default function DashboardSection() {
   const router = useRouter();
   const { section } = router.query;
 
-  return <main>{section === "projects" && <Projects />}</main>;
+  return (
+    <div>
+      <Sidebar />
+      {section === "projects" && <Projects />}
+      {section === "starred-projects" && <StarredProjects />}
+      {section === "templates" && <Templates />}
+      {section === "recently-deleted" && <RecentlyDeleted />}
+    </div>
+  );
 }
