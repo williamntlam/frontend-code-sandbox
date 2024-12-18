a7ff6bbab866aa1277a578fb558a082cca09dcdc
William Lam <williamntlam@gmail.com>
2024-12-17 19:41:09 -0500

    feat: wrapped entire app with ColourProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ColourProvider } from "@/context/ColourContext";
 
 export default function App({ Component }: AppProps) {
-  return <Component />;
+  return (
+    <ColourProvider>
+      <Component />
+    </ColourProvider>
+  );
 }
