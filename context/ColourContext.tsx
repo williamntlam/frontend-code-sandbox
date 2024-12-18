e28aaf3d9baf0a6662069479d96bf49eda58d028
William Lam <williamntlam@gmail.com>
2024-12-17 19:37:55 -0500

    feat: implemented ColourContext.tsx

--- context/ColourContext.tsx
+++ context/ColourContext.tsx
@@ -8,4 +8,48 @@ import React, {
   ReactNode,
 } from "react";
 
-type ColourMode = "light" | "dark";
+import { ColourMode, ColourContextType } from "@/interfaces/colourInterface";
+
+const ColourContext = createContext<ColourContextType | undefined>(undefined);
+
+export const ColourProvider = ({ children }: { children: ReactNode }) => {
+  const [mode, setMode] = useState<ColourMode>("light");
+
+  // Set the initial theme based on system preference.
+  useEffect(() => {
+    const savedMode = localStorage.getItem("theme") as ColourMode | null;
+    if (savedMode) {
+      setMode(savedMode);
+    } else {
+      const darkMode = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
+      setMode(darkMode ? "dark" : "light");
+    }
+  }, []);
+
+  // update localStorage and 'data-theme' attribute.
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", mode);
+    localStorage.setItem("theme", mode);
+  }, [mode]);
+
+  const toggleMode = () => {
+    setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ColourContext.Provider value={{ mode, toggleMode }}>
+      {children}
+    </ColourContext.Provider>
+  );
+};
+
+export const useColourMode = () => {
+  const context = useContext(ColourContext);
+  if (!context) {
+    throw new Error("useColourMode must be used within a ColourProvider");
+  }
+
+  return context;
+};
